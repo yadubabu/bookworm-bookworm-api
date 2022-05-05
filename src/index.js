ba6c34decb9dceb5d00c97212d0599026d688535
yadubabu <100501154+yadubabu@users.noreply.github.com>
2022-05-05 10:46:56 +0530

    Mongoose connecting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import auth from './routes/auth';
+import Promise from 'bluebird';
 
 
 const app=express();
+dotenv.config();
+app.use(bodyParser.json());
+mongoose.Promise=Promise;
+mongoose.connect("mongodb://localhost:27017/bookworm");
+app.use('/api/auth',auth);
 
 app.post('/api/auth',(req,res)=>{
     res.status(400).json({errors:{global:"Invalid Credentials"}});
